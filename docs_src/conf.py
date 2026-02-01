671659917da2038977660c394d2e32465393d4e9
Hirak Sarkar <hiraksarkar.cs@gmail.com>
2026-02-01 13:49:20 -0600

    Add examples to TOC and fix navigation settings to always expand

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -26,9 +26,7 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'collapse_navigation': False,
     'sticky_navigation': True,
-    'navigation_depth': -1,
-    "navigation_with_keys": False,
-    "show_nav_level": 2,
+    'navigation_depth': 5,
 }
 html_static_path = ['_static']
 
