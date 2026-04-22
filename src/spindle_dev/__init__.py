3edecb9515a45ed737ea26c05730d861240d48b5
sarkah1 <sarkah1@ROSALIND-LOGIN.vuds.vanderbilt.edu>
2026-04-22 15:17:15 +0000

    testing partial dydactic search

--- src/spindle_dev/__init__.py
+++ src/spindle_dev/__init__.py
@@ -8,3 +8,10 @@ from .index import IndexConfig, IndexHandle, build_index  # noqa: F401
 from .search import SearchConfig, SearchResults, query_index  # noqa: F401
 from .metrics import log_euclidean_distance, spd_to_correlation, correlation_to_spd  # noqa: F401
 from .utils import DeterministicConfig
+from .interval_index import (  # noqa: F401
+    build_all_interval_indices,
+    query_interval_index,
+    query_arbitrary_interval,
+    decompose_to_dyadic,
+    IntervalCluster,
+)
